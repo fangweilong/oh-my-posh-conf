a836e480061abacb0c31e1980cf839e474ea7d2b
fangweilong <fangweilong@outlook.com>
2021-12-16 14:00:20 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme stelbent.minimal
+#powershell安装方式
+#Import-Module oh-my-posh
+#Set-PoshPrompt -Theme stelbent.minimal
 # 提示?
-Enable-PoshTooltips
+#Enable-PoshTooltips
 # 主题
-Set-PoshPrompt -Theme my
+#Set-PoshPrompt -Theme my
+
+# winget安装方式
+oh-my-posh --init --shell pwsh --config C:\Users\teler\AppData\Local\Programs\oh-my-posh\themes\my.omp.json | Invoke-Expression
